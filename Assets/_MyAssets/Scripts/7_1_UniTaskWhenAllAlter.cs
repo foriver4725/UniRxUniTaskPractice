2e477b84acb141d11c308d2658f2deb0ed7bee67
foriver4725 <hrt67@icloud.com>
2025-11-16 00:01:29 +0900

    7-1

---
+++ Assets/_MyAssets/Scripts/7_1_UniTaskWhenAllAlter.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+namespace Section7_1
+{
+    public sealed class UniTaskWhenAllAlter : MonoBehaviour
+    {
+        private async UniTaskVoid Start()
+        {
+            var uniTask1 = UniTask.FromResult(1);
+            var uniTask2 = UniTask.FromResult(2);
+            var uniTask3 = UniTask.FromResult(3);
+
+            var (r1, r2, r3) = await (uniTask1, uniTask2, uniTask3);
+            Debug.Log((r1, r2, r3));
+
+            (r1, r2, r3) = await UniTask.WhenAll(uniTask1, uniTask2, uniTask3);
+            Debug.Log((r1, r2, r3));
+        }
+    }
+}
